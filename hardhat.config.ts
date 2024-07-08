96d71b28f6454f880caf48447360af6c905aa775
Kushal Pokharel <kusaalpokharel@gmail.com>
2024-07-09 00:02:29 +0545

    Add deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {PRIVATE_KEY} from './keyconfig';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    holesky: {
+      url: `https://endpoints.omniatech.io/v1/eth/holesky/public`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
